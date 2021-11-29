07f3f821159eaeca2de47a3fa746be634719dd49
Jacqueline Teixeira <teixeijf@gmai.com>
2021-11-29 19:42:24 -0300

    componente extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
-  declarations: [AppComponent, NovaTransferenciaComponent],
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BLR' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
